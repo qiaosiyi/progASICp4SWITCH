288538bc009dddb66e4daa44a7cc18579f7808e3
qiaosiyi <qsy2011815@163.com>
2019-03-20 17:07:34 +0800

    Update statefulltb.v

--- statefulltb.v
+++ statefulltb.v
@@ -7,29 +7,38 @@ wire [511:0] pkt_data_out;
 wire [15:0] action_out;
 wire [7:0] state_out;
 
-reg 		pkt_vld_in;
-reg [511:0] pkt_data_in;
-reg   clk;
-reg reset;
+reg 		pkt_vld_in=0;
+reg [511:0] pkt_data_in=0;
+reg   clk=1;
+reg reset=1;
 
 always begin
-		#5 clk = 1;
 		#5 clk = 0;
+		#5 clk = 1;
 	end
 
 	initial begin
 
 		#7 reset = 1;
 
-		#23 reset = 0;
+		#33 reset = 0;
 		
-		#10 pkt_vld_in = 1;pkt_data_in = 'h4329;
+		#10 pkt_vld_in = 1;pkt_data_in = 512'h4322;
 		#10 pkt_vld_in = 0;pkt_data_in = 0;
 		
-		#10 pkt_vld_in = 1;pkt_data_in = 'h4329;
+		#10 pkt_vld_in = 1;pkt_data_in = 512'h4322;
 		#10 pkt_vld_in = 0;pkt_data_in = 0;
 		
-		#10 pkt_vld_in = 1;pkt_data_in = 'h4329;
+		#10 pkt_vld_in = 1;pkt_data_in = 512'h4322;
+		#10 pkt_vld_in = 0;pkt_data_in = 0;
+				
+		#10 pkt_vld_in = 1;pkt_data_in = 512'h4322;
+		#10 pkt_vld_in = 0;pkt_data_in = 0;
+				
+		#10 pkt_vld_in = 1;pkt_data_in = 512'h4322;
+		#10 pkt_vld_in = 0;pkt_data_in = 0;
+				
+		#10 pkt_vld_in = 1;pkt_data_in = 512'h4322;
 		#10 pkt_vld_in = 0;pkt_data_in = 0;
 	end
 
@@ -41,7 +50,9 @@ always begin
 		.pkt_vld_out(pkt_vld_out),
 		.pkt_data_out(pkt_data_out),
 		.action_out(action_out),
-		.state_out(state_out)	
+		.state_out(state_out),
+		.reset(reset),
+		.clk(clk)
 	);
 	
 	
