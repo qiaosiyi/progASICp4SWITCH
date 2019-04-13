c21cfc7ea7fad768e0f27a00a04ef909ab733004
qiaosiyi <qsy2011815@163.com>
2019-04-13 22:52:24 +0800

    Update ndpqs.v

--- ndpqs.v
+++ ndpqs.v
@@ -59,9 +59,111 @@ module ndpqs
 			depth[6] <= 0;
 			depth[7] <= 0;
 		end else begin
-			if(depth[0] > 0) begin
-				
+			if(datavalid0)begin
+				if(depth[0] + len[0] - C_SEND_SPEED > 0)begin
+					depth[0] <= depth[0] + len[0] - C_SEND_SPEED;
+				end else begin
+					depth[0] <= 0;
+				end
+				if(depth[0] > C_MAX_DEPTH)begin
+					trans_len <= 0;
+				end
+			end else begin
+				if(depth[0] - C_SEND_SPEED > 0)begin
+					depth[0] <= depth[0] - C_SEND_SPEED;
+				end else begin
+					depth[0] <= 0;
+				end
 			end
+			if(datavalid1)begin
+				if(depth[1] + len[1] - C_SEND_SPEED > 0)begin
+					depth[1] <= depth[1] + len[1] - C_SEND_SPEED;
+				end else begin
+					depth[1] <= 0;
+				end
+				if(depth[1] > C_MAX_DEPTH)begin
+					trans_len <= 0;
+				end
+			end else begin
+				if(depth[1] - C_SEND_SPEED > 0)begin
+					depth[1] <= depth[1] - C_SEND_SPEED;
+				end else begin
+					depth[1] <= 0;
+				end
+			end
+			if(datavalid2)begin
+				if(depth[2] + len[2] - C_SEND_SPEED > 0)begin
+					depth[2] <= depth[2] + len[2] - C_SEND_SPEED;
+				end
+					depth[2] <= 0;
+			end else begin
+				if(depth[2] - C_SEND_SPEED > 0)begin
+					depth[2] <= depth[2] - C_SEND_SPEED;
+				end else begin
+					depth[2] <= 0;
+				end
+			end
+			if(datavalid3)begin
+				if(depth[3] + len[3] - C_SEND_SPEED > 0)begin
+					depth[3] <= depth[3] + len[3] - C_SEND_SPEED;
+				end
+					depth[3] <= 0;
+			end else begin
+				if(depth[3] - C_SEND_SPEED > 0)begin
+					depth[3] <= depth[3] - C_SEND_SPEED;
+				end else begin
+					depth[3] <= 0;
+				end
+			end
+			if(datavalid4)begin
+				if(depth[4] + len[4] - C_SEND_SPEED > 0)begin
+					depth[4] <= depth[4] + len[4] - C_SEND_SPEED;
+				end
+					depth[4] <= 0;
+			end else begin
+				if(depth[4] - C_SEND_SPEED > 0)begin
+					depth[4] <= depth[4] - C_SEND_SPEED;
+				end else begin
+					depth[4] <= 0;
+				end
+			end
+			if(datavalid5)begin
+				if(depth[0] + len[5] - C_SEND_SPEED > 0)begin
+					depth[5] <= depth[5] + len[5] - C_SEND_SPEED;
+				end
+					depth[5] <= 0;
+			end else begin
+				if(depth[5] - C_SEND_SPEED > 0)begin
+					depth[5] <= depth[5] - C_SEND_SPEED;
+				end else begin
+					depth[5] <= 0;
+				end
+			end
+			if(datavalid6)begin
+				if(depth[6] + len[6] - C_SEND_SPEED > 0)begin
+					depth[6] <= depth[6] + len[6] - C_SEND_SPEED;
+				end
+					depth[6] <= 0;
+			end else begin
+				if(depth[6] - C_SEND_SPEED > 0)begin
+					depth[6] <= depth[6] - C_SEND_SPEED;
+				end else begin
+					depth[6] <= 0;
+				end
+			end
+			if(datavalid7)begin
+				if(depth[7] + len[7] - C_SEND_SPEED > 0)begin
+					depth[7] <= depth[7] + len[7] - C_SEND_SPEED;
+				end
+					depth[7] <= 0;
+			end else begin
+				if(depth[7] - C_SEND_SPEED > 0)begin
+					depth[7] <= depth[7] - C_SEND_SPEED;
+				end else begin
+					depth[7] <= 0;
+				end
+			end
+
 		end
 	end
 
