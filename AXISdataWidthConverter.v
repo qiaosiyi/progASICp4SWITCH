954614dc42157a3ccbdcab9a7e8c937039749f2b
qiaosiyi <qsy2011815@163.com>
2019-11-10 15:32:27 +0800

    Update AXISdataWidthConverter.v

--- AXISdataWidthConverter.v
+++ AXISdataWidthConverter.v
@@ -13,5 +13,19 @@ module axis512to256#(
   input out_TREADY,
   output reg [C_WIDTH_TDATA/2-1:0] out_TDATA,
   output reg [C_WIDTH_TKEEP/2-1:0] out_TKEEP,
-  output reg out_TLAST
+  output reg out_TLAST, 
+  input rst,
+  input clk
 );
+  
+localparam S_IDLE = 0;
+localparam S_TRANSA = 1;
+localparam S_TRANSB = 2;
+localparam S_LASTA = 3;
+localparam S_LASTB = 4;
+  
+  reg [8:0] pre_state, next_state;
+  
+  reg in_buf_fifo_rd_en;
+  wire [C_WIDTH_TDATA-1:0] in_buf_fifo_dout_TDATA;
+  wire 
