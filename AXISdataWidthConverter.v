9229395d13498bac198980c72e0b1dfc39234dbe
qiaosiyi <qsy2011815@163.com>
2019-11-11 08:50:34 +0800

    Update AXISdataWidthConverter.v

--- AXISdataWidthConverter.v
+++ AXISdataWidthConverter.v
@@ -4,25 +4,27 @@ module axis2wtow#(
   parameter C_WIDTH_TDATA = 512,
   parameter C_WIDTH_TKEEP = C_WIDTH_TDATA / 8
 )(
-  input in_TVALID,
-  output in_TREADY,
-  input [C_WIDTH_TDATA-1:0] in_TDATA,
-  input [C_WIDTH_TKEEP-1:0] in_TKEEP,
-  input in_TLAST,
-  output reg out_TVALID,
-  input out_TREADY,
-  output reg [C_WIDTH_TDATA/2-1:0] out_TDATA,
-  output reg [C_WIDTH_TKEEP/2-1:0] out_TKEEP,
-  output reg out_TLAST, 
-  input rst,
-  input clk
+  input                                                              in_TVALID,
+  output                                                             in_TREADY,
+  input                                          [C_WIDTH_TDATA-1:0] in_TDATA,
+  input                                          [C_WIDTH_TKEEP-1:0] in_TKEEP,
+  input                                                              in_TLAST,
+
+  output reg                                                         out_TVALID,
+  input                                                              out_TREADY,
+  output reg                                   [C_WIDTH_TDATA/2-1:0] out_TDATA,
+  output reg                                   [C_WIDTH_TKEEP/2-1:0] out_TKEEP,
+  output reg                                                         out_TLAST,
+
+  input                                                              clk_line_rst,
+  input                                                              clk_line
 );
   
-localparam S_IDLE = 0;
-localparam S_TRANSA = 1;
-localparam S_TRANSB = 2;
-localparam S_LASTA = 3;
-localparam S_LASTB = 4;
+localparam                                                           S_IDLE = 0;
+localparam                                                           S_TRANSA = 1;
+localparam                                                           S_TRANSB = 2;
+localparam                                                           S_LASTA = 3;
+localparam                                                           S_LASTB = 4;
   
 reg                                                                  in_buffer_fifo_rd_en;
 wire                                             [C_WIDTH_TDATA-1:0] in_buffer_fifo_dout_TDATA;
@@ -34,7 +36,7 @@ wire                                                                 in_buffer_f
 reg                                              [C_WIDTH_TDATA-1:0] out_buf_TDATA;
 reg                                              [C_WIDTH_TKEEP-1:0] out_buf_TKEEP;
 reg                                                                  last_done;
-
+reg                                                           [8:0]  pre_state, next_state;
 
 assign in_TREADY = !in_buffer_fifo_nearly_full;
   
